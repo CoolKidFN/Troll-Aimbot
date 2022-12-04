88341b37e33e866018cb828d3172b3146fd4f261
CoolKidFN <119802865+CoolKidFN@users.noreply.github.com>
2022-12-04 16:41:47 +0100

    Aimbot.bat
    V1.0(Alpha)

---
+++ Aimbot.bat
@@ -0,0 +1,9 @@
+@echo off
+echo Aimbot By Jarvis
+echo ====================================================
+timeout /t 1 /nobreak > nul
+echo injecting
+timeout /t 20 /nobreak > nul
+echo injected
+pause
+start https://www.youtube.com/watch?v=dQw4w9WgXcQ
